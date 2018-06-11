99e0b11f7bf5d447aaa53cd411301e9b08025b93
Unknown <maura.sacca@gmail.com>
2018-06-11 21:07:21 +0200

    Cambi nei prefab di Player e Enemy

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
-{
+{   
+    public float _damage = 10;
 
     private void Start()
     {
         
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
+    private void OnCollisionEnter(Collision other) //cambiato da collision a collider
+    {   
         Destroy(this.gameObject);
+    }
+
+    private void OnColliderEnter(Collider other) {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<Health>().TakeDamage(_damage);
+        }
     }
 }
